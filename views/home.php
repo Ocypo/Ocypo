4df728a946a6253b5a8cb483149aaf34a80a5e4f
Niels Meijer <nielsmeijer@hotmail.com>
2014-03-27 00:20:35 +0100

    Added example for VIEW::getTitle()

--- views/home.php
+++ views/home.php
@@ -4,6 +4,14 @@
 #
 # It is also possible to use $args to get all arguments passed to the view.
 # This could be quite usefull when you want to pass them through via json.
-
-echo $text;
 ?>
+<html>
+<head>
+  <title><?php echo VIEW::getTitle(); ?></title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="copyright" content="NielsMeijer.eu" />
+</head>
+<body>
+<?php echo $text; ?>
+</body>
+</html>
